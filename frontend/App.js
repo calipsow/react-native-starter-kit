f8932eb031e0a178126b5f4ef43e5475ba82c00f
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-05 16:30:56 +0200

    App.js aktualisieren

--- frontend/App.js
+++ frontend/App.js
@@ -2,44 +2,44 @@ import React, { createContext, useEffect, useState } from 'react';
 import { ActivityIndicator, Platform, StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { colors } from './src/styles/colors';
+import { colors } from './src/styles/colors'; // TODO dark light mode
 
 import { persistor, store } from './src/redux/store';
 
-import { app, auth, db, storage } from './config/firebase-client';
+import { app, auth, db, storage } from './config/firebase-client'; // TODO remove storage by default
 import AppView from './src/modules/AppViewContainer';
 
 import { PermissionsAndroid } from 'react-native';
 import { useSafeArea } from 'react-native-safe-area-context';
-import useCheckAppVersionCompatibility from './src/hooks/app/use-check-app-version-compatibilty';
-import { BugReportProvider } from './src/modules/bug-report/BugReportProvider';
+import useCheckAppVersionCompatibility from './src/hooks/app/use-check-app-version-compatibilty'; // TODO remove this dep
+import { BugReportProvider } from './src/modules/bug-report/BugReportProvider'; // TODO remove debug context
 import {
   BugReportButton,
   ModalContent,
-} from './src/modules/bug-report/ReportComponents';
+} from './src/modules/bug-report/ReportComponents'; // TODO remove 
 import { ModalProvider } from './src/modules/provider/ModalProvider';
-
+// TODO dont call it that way
 Platform.OS === 'android' &&
   PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 
 export const Firebase = createContext({
   auth: null,
   db: null,
-  storage: null,
+  storage: null, // todo renove 
   app: null,
 });
 
 function App({ top = 0, right = 0 }) {
   const [firebase, setFirebase] = useState({});
   useEffect(() => {
-    if (firebase.auth && firebase.db && firebase.storage) return;
+    if (firebase.auth && firebase.db && firebase.storage) return; // todo remove storage
     setFirebase({
       auth,
       db,
-      storage,
+      storage, // todo remove
       app,
     });
-  }, [auth, db, storage]);
+  }, [auth, db, storage]); // todo
 
   return (
     <BugReportProvider>
@@ -77,7 +77,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: 'white', // todo possible unwanted static bg
   },
 });
 
