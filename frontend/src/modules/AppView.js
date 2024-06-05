fc43cc8b96a3bd692233964389998119dffde92d
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-05 16:35:13 +0200

    AppView.js aktualisieren

--- frontend/src/modules/AppView.js
+++ frontend/src/modules/AppView.js
@@ -1,4 +1,4 @@
-import { ShopifyCheckoutSheetProvider } from '@shopify/checkout-sheet-kit';
+import { ShopifyCheckoutSheetProvider } from '@shopify/checkout-sheet-kit'; // remove lib
 import React, { createContext, useState } from 'react';
 import useAuthListener from '../hooks/auth/use-auth-listener';
 import useSyncAccountChanges from '../hooks/context/use-change-listener';
@@ -9,7 +9,9 @@ export const AccountContext = createContext([]);
 
 export default function AppView() {
   const [accountContext, setAccountContext] = useState(null);
+  // create documentation for this hook
   useSyncAccountChanges(accountContext); // syncs automatic the accountCtx state with the user data in the firestore db
+  // refactor this hook and document it
   useAuthListener(accountContext, setAccountContext); // syncs the accountCtx with the firebase auth state if the auth state changes
   // it creates also a new account if no one is found
 
