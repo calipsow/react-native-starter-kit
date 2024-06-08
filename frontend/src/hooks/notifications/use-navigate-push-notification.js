9755c49a0e3b60bf19c322cfceb5aa5cc665a4f9
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-08 18:10:34 +0200

    use-navigate-push-notification.js aktualisieren

--- frontend/src/hooks/notifications/use-navigate-push-notification.js
+++ frontend/src/hooks/notifications/use-navigate-push-notification.js
@@ -2,7 +2,6 @@ import messaging from '@react-native-firebase/messaging';
 import { useNavigation } from '@react-navigation/native';
 import React, { useEffect } from 'react';
 
-// Funktion zum Umgang mit Push-Benachrichtigungen
 function useHandlePushNotifications() {
   const navigation = useNavigation();
 
@@ -11,12 +10,11 @@ function useHandlePushNotifications() {
       handlePushNotification(remoteMessage, navigation);
     });
 
-    // Wenn die App im Hintergrund gestartet wird
+    // When the app is started in the background
     messaging()
       .getInitialNotification()
       .then(remoteMessage => {
         if (remoteMessage) {
-          // console.log(remoteMessage);
           handlePushNotification(remoteMessage, navigation);
         }
       });
@@ -25,25 +23,22 @@ function useHandlePushNotifications() {
   }, [navigation]);
 }
 
-// Funktion zum Verarbeiten der Push-Benachrichtigung
+
 function handlePushNotification(remoteMessage, navigation) {
   // if (!remoteMessage?.data?.data) return;
   try {
     const values = JSON.parse(remoteMessage.data.data);
-    console.log('\n\n', values);
+    console.log('push notification data ->', values);
     if (values) {
-      switch (values.type) {
-        case 'admin':
-          navigation.navigate(values.targetScreen, { screen: 'Home' });
+      switch (true) {
+        case !values.targetScreen:
+          console.warn('no targetScreen was specified in the push notification data')
           break;
-        case 'event':
-          navigation.navigate(values.targetScreen, values.params);
-          break;
-        case 'article':
-          navigation.navigate(values.targetScreen, values.params);
+        case !values.params:
+          navigation.navigate(values.targetScreen);
           break;
         default:
-          navigation.navigate(values.targetScreen);
+          navigation.navigate(values.targetScreen, values.params);
           break;
       }
     }
