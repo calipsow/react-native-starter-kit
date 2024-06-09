447b27bb640ada373b4dc90571921c56ea398189
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-09 06:05:03 +0200

    use-delete-document.js aktualisieren

--- frontend/src/hooks/firebase/use-delete-document.js
+++ frontend/src/hooks/firebase/use-delete-document.js
@@ -11,18 +11,14 @@ const useDeleteDocumentIfAdmin = () => {
 
   const deleteDocument = async (collection, documentId) => {
     try {
-      // Dokument aus der angegebenen Collection löschen
       const documentRef = doc(db, collection, documentId);
-
-      // Dokument aus der Firestore-Datenbank löschen
       await deleteDoc(documentRef);
-
       setSuccess(true);
       setPermissionState(true);
     } catch (error) {
       console.warn('Fehler beim Löschen des Dokuments: ', error);
       setSuccess(false);
-      setPermissionState(true); // Der Nutzer hatte die Berechtigung, aber es gab einen Fehler beim Löschen
+      setPermissionState(true);
     }
   };
 
