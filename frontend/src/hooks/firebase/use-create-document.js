d8f8507c7ec0fae7687c72d2dbe0909fd6c5ed60
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-09 06:04:20 +0200

    use-create-document.js aktualisieren

--- frontend/src/hooks/firebase/use-create-document.js
+++ frontend/src/hooks/firebase/use-create-document.js
@@ -1,23 +1,22 @@
 import { useState, useContext } from 'react';
-import { Firebase } from '../../../App'; // Stellen Sie sicher, dass dieser Pfad korrekt ist
+import { Firebase } from '../../../App'; 
 import { collection, doc, getFirestore, setDoc } from 'firebase/firestore';
 
 const useCreateDocument = () => {
-  const { db } = useContext(Firebase); // Stellen Sie sicher, dass der Context korrekt verwendet wird
+  const { db } = useContext(Firebase); 
   const [succeeded, setSucceeded] = useState(false);
   const [error, setError] = useState('');
 
   const createDocument = async (collectionID, documentId, data) => {
     try {
-      // Prüfe, ob ein documentId vorhanden ist, um ein spezifisches Dokument zu erstellen oder zu überschreiben
       const docRef = documentId
         ? doc(getFirestore(), collectionID, documentId)
         : doc(collection(getFirestore(), collectionID));
-      await setDoc(docRef, data, { merge: true }); // 'merge: true' fügt Daten zu einem bestehenden Dokument hinzu oder erstellt ein neues Dokument, wenn es nicht existiert
+      await setDoc(docRef, data, { merge: true }); // 'merge: true' adds data to the doc or create one if nothing was found
 
       setSucceeded(true);
     } catch (err) {
-      console.error('Fehler beim Erstellen des Dokuments: ', err);
+      console.error('Error creating the document:', err);
       setError(err.message);
       setSucceeded(false);
     }
