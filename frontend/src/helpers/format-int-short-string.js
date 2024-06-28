e36f306b2c3a52bfc358557b2595e66e4348323a
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-28 13:30:02 +0200

    format-int-short-string.js aktualisieren

--- frontend/src/helpers/format-int-short-string.js
+++ frontend/src/helpers/format-int-short-string.js
@@ -1,20 +1,17 @@
 export function formatNumberShort(number) {
   if (!number) return number;
   const thresholds = [
-    { value: 1_000_000_000, symbol: 'B' }, // Milliarde
-    { value: 1_000_000, symbol: 'M' }, // Million
-    { value: 1_000, symbol: 'K' }, // Tausend
+    { value: 1_000_000_000, symbol: 'B' }, // Billion 
+    { value: 1_000_000, symbol: 'M' }, // Million 
+    { value: 1_000, symbol: 'K' }, // Thausend 
   ];
 
-  // Wenn die Zahl kleiner als 1.000 ist, einfach als String zurückgeben
   if (number < 1_000) {
     return number.toString();
   }
 
-  // Finde das passende Schwellenwertobjekt, um die Abkürzung zu bestimmen
   const threshold = thresholds.find(th => number >= th.value);
 
-  // Berechne den abgekürzten Wert
   const shortenedValue = (number / threshold.value).toFixed(1); // Ein Nachkommastelle
   const result = `${shortenedValue}${threshold.symbol}`;
 
