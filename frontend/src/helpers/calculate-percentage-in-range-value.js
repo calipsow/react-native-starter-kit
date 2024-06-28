b133dc61a72644c03b3e3d6e86dc984e568f98c3
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-28 13:28:53 +0200

    calculate-percentage-in-range-value.js aktualisieren

--- frontend/src/helpers/calculate-percentage-in-range-value.js
+++ frontend/src/helpers/calculate-percentage-in-range-value.js
@@ -12,27 +12,25 @@ function calculatePercentageInRange(
   min_value,
   calculate_on_range,
 ) {
-  // Berechnung des Prozentsatzes von given_value im Bereich von min_value bis max_value
+
   const percentage = (given_value - min_value) / (max_value - min_value);
 
-  // Anwenden des berechneten Prozentsatzes auf den neuen Bereich, definiert durch calculate_on_range
+  // Applying the calculated percentage to the new range, defined by calculate_on_range
   const result =
     calculate_on_range.start +
     percentage * (calculate_on_range.end - calculate_on_range.start);
 
-  // Rückgabe des berechneten Werts
-  // console.log(result);
   return result;
 }
 
-/* Beispiel, wie die Funktion verwendet werden kann:
+/* Example of how the function can be used:
 const max_value = 10;
 const min_value = 0;
 const given_value = 5;
 const range = { zero: -120, hundred: 120 };
 
 const output = calculatePercentageInRange(max_value, given_value, min_value, range);
-console.log(output);  // Dies wird den umgerechneten Wert im neuen Bereich ausgeben
+console.log(output);  // This will output the converted value in the new area
 */
 
 export default calculatePercentageInRange;
