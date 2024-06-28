f00daf3cba080e01771a78b49b8db9535b3abb88
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-28 13:27:00 +0200

    is-past-timestamp.js aktualisieren

--- frontend/src/helpers/is-past-timestamp.js
+++ frontend/src/helpers/is-past-timestamp.js
@@ -1,15 +1,12 @@
 export default function isPastInGermany(timestamp) {
-  // Aktuelle Zeit in Deutschland (MEZ/MESZ)
+
   const now = new Date();
   const currentTimezoneOffset = new Date().getTimezoneOffset();
-  const germanyOffset = 60; // Deutschland ist UTC +1 (im Winter) oder +2 (im Sommer)
+  const germanyOffset = 60; 
 
-  // Zeitzonenunterschied korrigieren
   const adjustedOffset = germanyOffset - currentTimezoneOffset / 60;
 
-  // Verschiebung auf die deutsche Zeit
   const germanyTime = new Date(now.getTime() + adjustedOffset * 60 * 60 * 1000);
 
-  // Vergleiche den gegebenen Timestamp
   return timestamp < new Date().getTime();
 }
