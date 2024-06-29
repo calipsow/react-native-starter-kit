69107793f5f2b8b93cfd8cab8f153e1d27f7487c
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-29 06:10:49 +0200

    shuffle-array.js aktualisieren

--- frontend/src/helpers/shuffle-array.js
+++ frontend/src/helpers/shuffle-array.js
@@ -1,20 +1,14 @@
 export default function shuffleArray(array = []) {
   if (!Array.isArray(array) || !array.length) return [];
-  // Kopiere das übergebene Array, um die Originaldaten unverändert zu lassen
+
   let shuffledArray = array.slice();
 
-  // Gehe das Array von hinten durch
   for (let i = shuffledArray.length - 1; i > 0; i--) {
-    // Wähle ein zufälliges Element aus den noch nicht durchlaufenen Elementen
     const randomIndex = Math.floor(Math.random() * (i + 1));
-
-    // Tausche das aktuelle Element mit dem zufällig gewählten Element
     [shuffledArray[i], shuffledArray[randomIndex]] = [
       shuffledArray[randomIndex],
       shuffledArray[i],
     ];
   }
-
-  // Gebe das gemischte Array zurück
   return shuffledArray;
 }
