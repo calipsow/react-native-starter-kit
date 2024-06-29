25a5b2597873fbac6ae035063014334c0d1d6e57
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-29 06:04:55 +0200

    replace-umlaute.js aktualisieren

--- frontend/src/helpers/replace-umlaute.js
+++ frontend/src/helpers/replace-umlaute.js
@@ -1,6 +1,5 @@
 function replaceUmlauts(str) {
   if (typeof str !== 'string') return str;
-  // Mapping-Objekt für Umlaute und Sonderzeichen
   const umlautMapping = {
     ä: 'ae',
     ö: 'oe',
@@ -8,18 +7,13 @@ function replaceUmlauts(str) {
     Ä: 'Ae',
     Ö: 'Oe',
     Ü: 'Ue',
-    ß: 'ss', // Falls du auch "ß" in "ss" umwandeln möchtest
+    ß: 'ss', 
   };
-
-  // Iteriere über das Mapping-Objekt und ersetze alle vorkommenden Zeichen
   let result = str;
-
   for (const [umlaut, replacement] of Object.entries(umlautMapping)) {
-    // Verwende RegExp, um alle Vorkommen des Zeichens zu ersetzen
-    const regex = new RegExp(umlaut, 'g'); // 'g' steht für global (alle Vorkommen)
+    const regex = new RegExp(umlaut, 'g'); 
     result = result.replace(regex, replacement);
   }
-
-  return result; // Gib den modifizierten String zurück
+  return result; 
 }
 export default replaceUmlauts;
