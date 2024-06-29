b3de9b40c0337afd11b7e839839de1218fba5101
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-29 06:13:29 +0200

    delete-document-async.js aktualisieren

--- frontend/src/functions/firestore/delete-document-async.js
+++ frontend/src/functions/firestore/delete-document-async.js
@@ -9,11 +9,16 @@ import {
 } from 'firebase/firestore';
 
 /**
- * Schreibt oder aktualisiert ein Dokument in Firestore.
- * @param {string} collectionID - Die ID der Sammlung, in der das Dokument sich befindet.
- * @param {string} docID - Die ID des Dokuments, das gelöscht werden soll.
- * @returns {Promise<void>} Ein Promise, das abgeschlossen wird, wenn die Operation erfolgreich war.
- */
+
+* Writes or updates a document in Firestore.
+
+* @param {string} collectionID - The ID of the collection in which the document is located.
+
+* @param {string} docID - The ID of the document to be deleted.
+
+* @returns {Promise<void>} A promise that is completed when the operation was successful.
+
+*/
 const deleteDocument = async (collectionID, docID) => {
   const db = getFirestore();
   try {
@@ -21,7 +26,7 @@ const deleteDocument = async (collectionID, docID) => {
     console.log('Document successfully deleted!');
   } catch (error) {
     console.error('Error deleting document: ', error);
-    throw error; // Weitergabe des Fehlers an den Aufrufer
+    throw error;
   }
 };
 
