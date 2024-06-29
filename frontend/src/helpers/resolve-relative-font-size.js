b472c1672e8dcf6b988c8d174a83990a27212505
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-29 06:06:18 +0200

    resolve-relative-font-size.js aktualisieren

--- frontend/src/helpers/resolve-relative-font-size.js
+++ frontend/src/helpers/resolve-relative-font-size.js
@@ -1,6 +1,5 @@
 import { PixelRatio } from 'react-native';
 
 const fontScale = PixelRatio.getFontScale();
-// console.log(fontScale);
 const getFontSize = size => size / fontScale;
 export default getFontSize;
