3a8d9b0481d814b5c27bf7961f6a287dd088f8f7
calipsow <86490046+calipsow@users.noreply.github.com>
2024-06-29 06:03:58 +0200

    parse-deeplink.js aktualisieren

--- frontend/src/helpers/parse-deeplink.js
+++ frontend/src/helpers/parse-deeplink.js
@@ -4,16 +4,16 @@
  * @returns an array [route, params]
  */
 export default function parseRouteAndParams(url = '') {
-  // Prüfe, welches Schema verwendet wird
+  // Adjust this so it matches your domain 
   try {
     let route = null,
       params = null;
     if (url.startsWith('shipnative://')) {
-      // Extrahiere alles nach "zsw://"
+
       route = url.split('shipnative://')[1].split('/')[0] || null;
       params = url.split('shipnative://')[1].split('/')[1] || null;
     } else if (url.startsWith('https://')) {
-      // Extrahiere alles nach "https://zusammen-stehen-wir.de/app/"
+
       route =
         url.split('https://shipnative.app/')[1].split('/')[0] ||
         null;
